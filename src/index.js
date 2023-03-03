e9a044bd9047565563b7027c74fc9341ff236dd8
Rohit Mondal <rray524@gmail.com>
2023-03-03 09:38:50 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Join />,
+  },
+  {
+    path: "/join",
+    element: <Join />,
+  },
+  {
+    path: "/chat",
+    element: <Chat />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
